29a3d18d4c6106adf9cf65cdc2e4f87d4882137d
Ainsley Nguyenho <anguyenho@lcs.on.ca>
2025-01-20 09:42:24 -0500

    Initial Commit

---
+++ LeapYearChecker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LeapYearChecker
+//
+//  Created by Ainsley Quyen Nguyenho on 2025-01-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LeapYearChecker/LeapYearCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeapYearCheckerApp.swift
+//  LeapYearChecker
+//
+//  Created by Ainsley Quyen Nguyenho on 2025-01-20.
+//
+
+import SwiftUI
+
+@main
+struct LeapYearCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
