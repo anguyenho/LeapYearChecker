17ab987497990edd7445d006dc32192236903f4c
Ainsley Nguyenho <anguyenho@lcs.on.ca>
2025-01-22 11:47:03 -0500

    Added check and reset buttons

--- LeapYearChecker/Views/LeapYearCheckerView.swift
+++ LeapYearChecker/Views/LeapYearCheckerView.swift
@@ -8,16 +8,47 @@
 import SwiftUI
 
 struct LeapYearCheckerView: View {
+    
+    // MARK: Stored properties
+    // What year the user has stated
     @State private var year = ""
+    
+    // MARK: Computed properties
     var body: some View {
         VStack {
            
             HStack {
                 TextField("Enter year", text: $year)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
+                
+            }
+            .padding()
+            HStack {
+                Button(action: {}) {
+                    Text("Check")
+                        .padding(9)
+                        .foregroundColor(.white)
+                        .background(Color.blue)
+                        .cornerRadius(10)
+                    
+                    Button(action: {}) {
+                        Text("Reset")
+                            .padding(9)
+                            .foregroundStyle(.white)
+                            .background(Color.red)
+                            .cornerRadius(10)
+                    }
+                    HStack {
+                        // Print the result
+                        
+                        
+                    }
+                }
+                    
             }
         }
         .padding()
+        .navigationTitle("Leap Year Checker")
     }
 }
 
